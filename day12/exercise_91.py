f33904599647a6b5eb4287e0363cb0af07a90f37
gustavo <gustavo.perez.231191@gmail.com>
2022-06-28 19:19:03 -0500

    day 12 python exercises

---
+++ day12/exercise_91.py
@@ -0,0 +1,9 @@
+# Please write a program which accepts a string from console and print it in reverse order.
+# Example: If the following string is given as input to the program:*
+# rise to vote sir
+# Then, the output of the program should be:
+# ris etov ot esir
+
+sentence = input("Enter sentence: \n")
+
+print(sentence[::-1])
